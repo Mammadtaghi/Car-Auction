7a7931d238787782708656144041b28cad28ef82
Mammadtaghi <tu7k3zlya@code.edu.az>
2024-02-12 16:57:08 +0400

    ShopPage: 20%

--- client/src/Pages/Shop Page/index.jsx
+++ client/src/Pages/Shop Page/index.jsx
@@ -10,7 +10,66 @@ function ShopPage() {
                 <title>ShopPage</title>
             </Helmet>
             <div id={style.ShopPage}>
-                ShopPage
+                <div className={style.pageTitle}>
+                    <span className={style.path}>Shop /</span>
+                    <h1 className={style.filterText}>Shop: All</h1>
+                </div>
+                <div className={style.container}>
+                    <div className={style.filterBox}>
+                        <h2 className={style.filterBoxTitle}>Filters:</h2>
+
+                        <div className={style.filter}>
+                            <h3 className={style.filterTitle}>Car Models</h3>
+                            
+                            <div className={`${style.filters}`}>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>Audi</span>
+                                    <span className={style.count}>(6)</span>
+                                </div>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>BWM</span>
+                                    <span className={style.count}>(2)</span>
+                                </div>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>Dacia</span>
+                                    <span className={style.count}>(0)</span>
+                                </div>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>Pagani</span>
+                                    <span className={style.count}>(2)</span>
+                                </div>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>Ford</span>
+                                    <span className={style.count}>(4)</span>
+                                </div>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>Lotus</span>
+                                    <span className={style.count}>(1)</span>
+                                </div>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>Mercedes-Benz</span>
+                                    <span className={style.count}>(3)</span>
+                                </div>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>Chevrolet</span>
+                                    <span className={style.count}>(1)</span>
+                                </div>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>Mazda</span>
+                                    <span className={style.count}>(0)</span>
+                                </div>
+                                <div className={style.modelFilter}>
+                                    <span className={style.text}>Tesla</span>
+                                    <span className={style.count}>(2)</span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className={style.shopBox}>
+
+                    </div>
+                </div>
             </div>
         </>
     )
